a5d6a2435c572f47ec26a8bc5dd74ab92ba18014
aequiroga <94817156+aequiroga@users.noreply.github.com>
2022-05-05 17:14:37 -0300

    TP1 v1.1

--- TP1.c
+++ TP1.c
@@ -4,24 +4,28 @@
 void imprimirChar(char x);
 
 int main(int argc, char* argv[]) {
-
+    
     char x = 0;
-    if(argc<2) {//Si no hay argv, leo de consola
+    //Si no hay argv, leo de consola
+    if(argc<2) {
         printf("Terminar ejecucion con Ctrl+C\n");
         while(1) {
             if(x)imprimirChar(x);
             x = getchar();
         }
     }
+
+    //Si hay argv, leo del archivo
     else {
         FILE* file = fopen(argv[1],"r");
 
         if(file==NULL) printf("No se encontro el archivo");
-        else while(!feof(file)) {//Si hay argv, leo del archivo
+        else while(!feof(file)) {
             if(x)imprimirChar(x);
             fread(&x,sizeof(char),1,file);
         }
     }
+
     return 0;
 }
 
